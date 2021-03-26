4a6cb30c52334b853068d66e8b6061c3d9fd08fc
DinhSang <dinhsangdroid@gmail.com>
2021-03-26 11:57:15 +0700

    Stateful-DinhSang: ôn tập

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Welcome',
       home: Scaffold(
+        appBar: AppBar(title: Text('Example'),),
         body: Center(
           child: Text(wordPair.asPascalCase),
         ),
