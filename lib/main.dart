91feeed2b89ceab25ed2ed2064247a314f0db9e9
DinhSang <dinhsangdroid@gmail.com>
2021-03-26 11:55:10 +0700

    Stateful-DinhSang: ôn tập

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final wordPair = WordPair.random();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome',
+      home: Scaffold(
+        body: Center(
+          child: Text(wordPair.asPascalCase),
+        ),
+      ),
+    );
+  }
 }
